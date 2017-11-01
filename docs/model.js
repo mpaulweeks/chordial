73704e89ff189cf92e47e89e39afacfa3bff1d5e
mpaulweeks <mpaulweeks@gmail.com>
2017-11-01 00:24:16 -0400

    copy from scrap files

---
+++ docs/model.js
@@ -0,0 +1,69 @@
+const audioCtx = new AudioContext();
+
+class Note {
+  constructor(step) {
+    this.step = step;
+    const osc = audioCtx.createOscillator();
+    osc.frequency.value = 440 * Math.pow(2, step/12);
+    osc.connect(audioCtx.destination);
+    this.osc = osc;
+  }
+  play(when) {
+    this.osc.start(when);
+  }
+  stop(when) {
+    this.osc.stop(when);
+  }
+}
+
+class Chord {
+  constructor(steps, tonic, duration) {
+    tonic = tonic || 0;
+    this.notes = steps.map(step => {
+      return new Note(tonic + step);
+    });
+    this.duration = duration;
+  }
+  play(when) {
+    this.notes.forEach(note => {
+      note.play(when);
+    });
+  }
+  stop(when) {
+    this.notes.forEach(note => {
+      note.stop(when);
+    });
+  }
+}
+
+class Phrase {
+  constructor(tonic, phraseSteps, durations) {
+    this.tonic = tonic;
+    this.phraseSteps = phraseSteps;
+    this.durations = durations;
+    this.chords = null;
+  }
+  play() {
+    if (this.chords){
+      this.stop(chords);
+    }
+    this.chords = this.phraseSteps.map((chordSteps, ci) => {
+      return new Chord(chordSteps, this.tonic, this.durations[ci & this.phraseSteps.length]);
+    });
+
+    let start = audioCtx.currentTime;
+    this.chords.forEach(chord => {
+      const stop = start + chord.duration;
+      chord.play(start);
+      chord.stop(stop);
+      start = stop;
+    });
+  }
+  stop() {
+    if (this.chords){
+      this.chords.forEach(chord => {
+        chord.stop();
+      });
+    }
+  }
+}
---
+++ docs/view.js
@@ -0,0 +1,92 @@
+
+const domPlay = document.getElementById("play");
+const domStop = document.getElementById("stop");
+const domLoad = document.getElementById("load");
+const domKey = document.getElementById("key");
+const domSpeed = document.getElementById("speed");
+const domDurations = document.getElementById("durations");
+const domChords = document.getElementById("chords");
+let currPhrase = null;
+
+const loadPhrase = function(){
+  const tonic = parseFloat(domKey.value);
+  const speed = parseFloat(domSpeed.value);
+  const phraseSteps = domChords.value
+    .split('\n')
+    .map(ps => ps.trim())
+    .filter(ps => ps !== '' && !ps.startsWith('//'))
+    .map(ps => ps.split(',').map(cs => parseFloat(cs)));
+  const durations = domDurations.value
+    .split('\n')
+    .map(d => d.trim())
+    .filter(d => d !== '' && !d.startsWith('//'))
+    .map(d => parseFloat(d) * speed);
+
+  if (currPhrase){
+    currPhrase.stop();
+  }
+  currPhrase = new Phrase(tonic, phraseSteps, durations);
+}
+const playPhrase = function(){
+  currPhrase.play();
+}
+const stopPhrase = function(){
+  currPhrase.stop();
+}
+
+// generate HTML
+const tonics = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+for (var i = 0; i < 36; i++){
+  const value = i - 12;
+  const name = tonics[i % 12] + (1 + Math.floor(i/12));
+  domKey.innerHTML += `<option value="${value}">${name}</option>`;
+}
+
+// event listeners
+domPlay.addEventListener('click', playPhrase);
+domStop.addEventListener('click', stopPhrase);
+domLoad.addEventListener('click', loadPhrase);
+
+// defaults
+domKey.value = -10;
+domSpeed.value = `0.77`;
+domDurations.value = `
+// durations
+
+3
+1
+3
+1
+2
+2
+4
+
+`;
+domChords.value = `
+// chords for https://www.youtube.com/watch?v=lLD0Z_PRyQc&t=1m
+
+ 0, 4, 7
+-1, 4, 8
+ 0, 5, 9
+ 0, 4, 7
+-1, 2, 7
+ 0, 4, 7
+ 0, 5, 9
+
+// reference
+
+// I
+// 0,4,7
+
+// IV 64
+// 0,5,9
+
+// V 6
+// 2,7,11
+
+// V7
+// -5,-1,2,5
+`;
+
+// app start
+loadPhrase();
